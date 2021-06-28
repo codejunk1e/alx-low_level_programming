71b5a1d757b7eb0a9d5dfd115b890b9b5ffe4e10
codejunk1e <emeka14real@gmail.com>
2021-06-28 00:19:36 -0700

    Lint fixes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter 
+ * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98.
+ *
+ * @n: pointer to update
  *
  * Return: Always 0.
  */
