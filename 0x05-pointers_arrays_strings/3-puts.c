7298dae408221b7c2de164c6eb5bb17a1af44d4c
codejunk1e <emeka14real@gmail.com>
2021-06-28 02:23:48 -0700

    Function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts - Function that prints a string, followed by a new line.
+ *
+ * @str: String to be printed.
+ */
+
+void _puts(char *str)
+{
+
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+}
