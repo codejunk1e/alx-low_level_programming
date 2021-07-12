362a62b47f6c753e905608f14c1641a89c4a5d86
codejunk1e <emeka14real@gmail.com>
2021-07-12 13:44:43 -0700

    Indent updates

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,18 +2,16 @@
 #include "holberton.h"
 
 /**
- *  * main - print each argument passed to program on a line
- *   * @argc: argument counter
- *    * @argv: pointer to array of arguments
- *     * Return: 0 on success
- *      */
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-		int i = 0;
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 
-			while (i < argc)
-						printf("%s\n", argv[i++]);
-
-				return (0);
+	return (0);
 }
