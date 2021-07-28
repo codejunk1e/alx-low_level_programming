e4c4d194cd4fa92e853f4e6dc06920634d6874e8
codejunk1e <emeka14real@gmail.com>
2021-07-28 07:10:55 -0700

    Answer fixes!

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include<holberton.h>
+#include "holberton.h"
 
 /**
  * strcat - Concatenates the string pointed to by @src,
@@ -9,22 +9,15 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int index = 0, dest_len = 0;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
