7fc98d00a5f795bf3fd103130591e6f35e29115d
codejunk1e <emeka14real@gmail.com>
2021-08-02 20:39:10 -0700

    Fix errors for 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,13 @@
  *
  * Return: pointer to memory area s
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
-
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
 	return (dest);
 }
